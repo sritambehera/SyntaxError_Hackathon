b2087e019c602ac325bc797ebcc086a119fc0d5d
Sritam Behera <sritambehera110@gmail.com>
2019-09-27 21:30:51 +0530

    Added login, logout, sign up, change password

--- AllInOne/urls.py
+++ AllInOne/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.Home, name='base'),
+    url(r'^SignUp/$', views.SignUp, name = 'signup'),
+    url(r'^Login/$', views.Login, name = 'login'),
+    url(r'^Logout/$', views.Logout, name = 'logout'),
+    url(r'^ChangePassword/$', views.ChangePassword, name = 'changepassword')
 
   ]
--- AllInOne/views.py
+++ AllInOne/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout ,update_session_auth_hash
-from django.http import HttpResponse
+from django.contrib.auth import update_session_auth_hash
+
+
+
+
 def Home(request):
 	return render(request, 'AllInOne/home.html')
-'''
+
 def SignUp(request):
 	if request.method == "POST":
-		return HttpResponse(html)
-		form = UserCreationForm(data = request.POST)
+		form = UserCreationForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return render(request, 'AllInOne/signup.html')
+			return render(request, 'AllInOne/home.html')
 	else:
 		form = UserCreationForm()
-	return render(request,'AllInOne/signup.html', {'form':form})
-'''
-'''
+	return render(request, 'AllInOne/signup.html', {'form': form})
+
 def Login(request):
 	if request.method == 'POST':
 		form = AuthenticationForm(data=request.POST)
 		if form.is_valid():
 			user = form.get_user()
 			login(request, user)
-			return redirect('/AllInone/')
+			return redirect('/AllInOne/')
 	else:
 		form = AuthenticationForm()
 	return render(request, 'AllInOne/login.html', {'form':form})
@@ -51,4 +52,3 @@ def ChangePassword(request):
 	return render(request, 'AllInOne/changepassword.html', {'form':form})
 
 # Create your views here.
-'''
